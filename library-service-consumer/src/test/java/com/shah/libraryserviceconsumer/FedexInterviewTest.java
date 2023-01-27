665a1250c9b73a79e525228067bdc98a247befd0
norulshahlam <norulshahlam@gmail.com>
2023-01-27 10:56:37 +0800

    ss

---
+++ library-service-consumer/src/test/java/com/shah/libraryserviceconsumer/FedexInterviewTest.java
@@ -0,0 +1,13 @@
+package com.shah.libraryserviceconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
